58cda2ffab0629bb3f404bd4d0392bcd6e513d94
Adolph C <adolph.celestin@gmail.com>
2018-02-16 11:26:49 -0500

    Fills in stubbed out methods for token service.

--- trivia/postgres/token.go
+++ trivia/postgres/token.go
@@ -10,14 +10,40 @@ type tokenService struct {
 	db *sql.DB
 }
 
-func (s *tokenService) AuthTokenByString(token string) (*trivia.AuthToken, error) {
-	// #TODO not yet implemented.
-	return nil, nil
+func (s *tokenService) AuthTokenByString(tokenString string) (*trivia.AuthToken, error) {
+	token := &trivia.AuthToken{}
+	err := s.db.QueryRow("SELECT token, user_id, guest_id, expires_at FROM auth_tokens WHERE token = $1;", tokenString).Scan(
+		&token.Token,
+		&token.UserID,
+		&token.GuestID,
+		&token.ExpiresAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return token, nil
 }
 
 func (s *tokenService) CreateTokenPair(auth *trivia.AuthToken, refresh *trivia.RefreshToken) error {
-	// #TODO not yet implemented
-	return nil
+	return transact(s.db, func(tx *sql.Tx) error {
+		_, err := tx.Exec(
+			`INSERT INTO auth_tokens (token, user_id, guest_id, expires_at) VALUES ($1, $2, $3, $4)`,
+			auth.Token, auth.UserID, auth.GuestID, auth.ExpiresAt)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.Exec(
+			`INSERT INTO refresh_tokens (token, auth_token, user_id, guest_id, expires_at) VALUES ($1, $2, $3, $4, $5)`,
+			refresh.Token, refresh.AuthToken, refresh.UserID, refresh.GuestID, refresh.ExpiresAt)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
 
 // NewTokenService creats a use AuthTokenService
