377275d8ecda35ca19daa43f4f3fbebc2f48b4d9
Adolph C <adolph.celestin@gmail.com>
2018-02-16 11:02:35 -0500

    Moved the api package to the correct directory.

--- trivia/auth/handlers.go
+++ trivia/auth/handlers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/expixel/actual-trivia-server/trivia"
-	"github.com/expixel/actual-trivia-server/trivia/auth/api"
+	"github.com/expixel/actual-trivia-server/trivia/api"
 	"github.com/expixel/actual-trivia-server/trivia/auth/validate"
 )
 
