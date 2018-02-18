8fc28712c9951f35f6688b1abace810cea049039
Adolph C <adolph.celestin@gmail.com>
2018-02-17 22:23:37 -0500

    Implements marshaling methods for null.String

--- trivia/null/string.go
+++ trivia/null/string.go
@@ -2,9 +2,10 @@ package null
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"reflect"
 )
-
-// #TODO implement json and text marshalling for the string type.
 
 // String represents a nullable string
 type String struct {
@@ -14,4 +15,50 @@ type String struct {
 // NewString creates a new string marked as nonnull with the given value.
 func NewString(value string) String {
 	return String{sql.NullString{String: value, Valid: true}}
+}
+
+// MarshalJSON implements json.Marshaler for String
+func (s String) MarshalJSON() ([]byte, error) {
+	if !s.Valid {
+		return []byte("null"), nil
+	}
+	return []byte("\"" + s.String + "\""), nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler for String
+func (s *String) UnmarshalJSON(data []byte) (err error) {
+	var v interface{}
+	if err = json.Unmarshal(data, v); err != nil {
+		return
+	}
+
+	// #NOTE if I ever pass by this code again I should consider
+	// unmarshaling floats and integers too just to make this
+	// simpler and more flexible, but it might defeat the point
+	// of type safety, I don't know. -- Adolph C.
+	switch x := v.(type) {
+	case string:
+		s.Valid = true
+		s.String = x
+	case nil:
+		s.Valid = false
+	default:
+		err = fmt.Errorf("null: cannot unmarshal %v into null.String", reflect.TypeOf(v).Name())
+	}
+	return
+}
+
+// MarshalText implements TextMarshaler for String
+func (s String) MarshalText() ([]byte, error) {
+	if !s.Valid {
+		return []byte{}, nil
+	}
+	return []byte(s.String), nil
+}
+
+// UnmarshalText implements TextUnarmshaler for String
+func (s *String) UnmarshalText(text []byte) error {
+	s.Valid = true
+	s.String = string(text)
+	return nil
 }
